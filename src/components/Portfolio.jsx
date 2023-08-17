b5784382bdbda584b0fe0d8038b5b1e8e95f3a57
surendra1151 <surendrakalluri@Krishnas-MacBook-Pro.local>
2023-08-17 18:31:43 -0500

    Added netflix clone to portfolio

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,37 +1,88 @@
-import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSearchPlus } from '@fortawesome/free-solid-svg-icons';
-import { PopupboxManager, PopupboxContainer } from 'react-popupbox';
-import 'react-popupbox/dist/react-popupbox.css';
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearchPlus } from "@fortawesome/free-solid-svg-icons";
+import { PopupboxManager, PopupboxContainer } from "react-popupbox";
+import "react-popupbox/dist/react-popupbox.css";
 
 const Portfolio = () => {
+  const openPopupboxNetflix = () => {
+    const content = (
+      <>
+        <img
+          className="portfolio-image-popupbox"
+          src="images/netflix-image.png"
+          alt="Netflix App"
+        />
+        <h5 className="text-description">
+          Netflix Clone: an immersive, web-based streaming experience crafted
+          meticulously using React. This application replicates the core
+          features of the original Netflix, enabling users to explore a vast
+          array of movies and TV shows with ease.
+        </h5>
+        <b className="text-color">Live Demo:</b>{" "}
+        <a
+          className="hyper-link"
+          onClick={() =>
+            window.open(
+              "https://netflix-clone-krishna-kalluri-5rqge6hwu-krishna-kalluri.vercel.app/browse"
+            )
+          }
+        >
+          https://netflix-clone-krishna-kalluri-5rqge6hwu-krishna-kalluri.vercel.app/browse
+        </a>
+        <br />
+        <b className="text-color">Github:</b>{" "}
+        <a
+          className="hyper-link"
+          onClick={() =>
+            window.open("https://github.com/surendra1151/Netflix-Clone")
+          }
+        >
+          https://github.com/surendra1151/Netflix-Clone
+        </a>
+      </>
+    );
+
+    PopupboxManager.open({
+      content,
+      config: {
+        titleBar: {
+          enable: true,
+          text: "Netflix Clone App",
+        },
+        fadeIn: true,
+        fadeInSpeed: 500,
+      },
+    });
+  };
+
   const openPopupboxRestaurant = () => {
     const content = (
       <>
         <img
-          className='portfolio-image-popupbox'
-          src='images/restaurant.png'
-          alt='Indian-Restaurant App'
+          className="portfolio-image-popupbox"
+          src="images/restaurant.png"
+          alt="Indian-Restaurant App"
         />
-        <h5 className='text-description'>
+        <h5 className="text-description">
           It is a simple indian restaurant application which contains menu based
           on timings.
         </h5>
-        <b className='text-color'>Live Demo:</b>{' '}
+        <b className="text-color">Live Demo:</b>{" "}
         <a
-          className='hyper-link'
+          className="hyper-link"
           onClick={() =>
-            window.open('https://dazzling-golick-44c443.netlify.app/')
+            window.open("https://dazzling-golick-44c443.netlify.app/")
           }
         >
           https://dazzling-golick-44c443.netlify.app/
         </a>
         <br />
-        <b className='text-color'>Github:</b>{' '}
+        <b className="text-color">Github:</b>{" "}
         <a
-          className='hyper-link'
+          className="hyper-link"
           onClick={() =>
-            window.open('https://github.com/surendra1151/Indian-Restaurant')
+            window.open("https://github.com/surendra1151/Indian-Restaurant")
           }
         >
           https://github.com/surendra1151/Indian-Restaurant
@@ -44,7 +95,7 @@ const Portfolio = () => {
       config: {
         titleBar: {
           enable: true,
-          text: 'Indian-Restaurant APP',
+          text: "Indian-Restaurant APP",
         },
         fadeIn: true,
         fadeInSpeed: 500,
@@ -54,7 +105,7 @@ const Portfolio = () => {
   const popupboxConfigRestaurant = {
     titleBar: {
       enable: true,
-      text: 'Indian-Restaurant APP',
+      text: "Indian-Restaurant APP",
     },
     fadeIn: true,
     fadeInSpeed: 500,
@@ -64,29 +115,29 @@ const Portfolio = () => {
     const content = (
       <>
         <img
-          className='portfolio-image-popupbox-notes'
-          src='images/Note-taker.png'
-          alt='Note-Taker App'
+          className="portfolio-image-popupbox-notes"
+          src="images/Note-taker.png"
+          alt="Note-Taker App"
         />
-        <h5 className='text-description'>
+        <h5 className="text-description">
           It is a simple Note-taker which can add items list to our local
           storage.
         </h5>
-        <b className='text-color'>Live Demo:</b>{' '}
+        <b className="text-color">Live Demo:</b>{" "}
         <a
-          className='hyper-link'
+          className="hyper-link"
           onClick={() =>
-            window.open('https://clever-bassi-e230d1.netlify.app/')
+            window.open("https://clever-bassi-e230d1.netlify.app/")
           }
         >
           https://clever-bassi-e230d1.netlify.app/
         </a>
         <br />
-        <b className='text-color'>Github:</b>{' '}
+        <b className="text-color">Github:</b>{" "}
         <a
-          className='hyper-link'
+          className="hyper-link"
           onClick={() =>
-            window.open('https://github.com/surendra1151/Note-Taker')
+            window.open("https://github.com/surendra1151/Note-Taker")
           }
         >
           https://github.com/surendra1151/Note-Taker
@@ -99,7 +150,7 @@ const Portfolio = () => {
       config: {
         titleBar: {
           enable: true,
-          text: 'Note-Taker APP',
+          text: "Note-Taker APP",
         },
         fadeIn: true,
         fadeInSpeed: 500,
@@ -109,7 +160,7 @@ const Portfolio = () => {
   const popupboxConfigNotes = {
     titleBar: {
       enable: true,
-      text: 'Note-Taker APP',
+      text: "Note-Taker APP",
     },
     fadeIn: true,
     fadeInSpeed: 500,
@@ -119,28 +170,28 @@ const Portfolio = () => {
     const content = (
       <>
         <img
-          className='portfolio-image-popupbox'
-          src='images/tic-tac-toe.png'
-          alt='Tic-Tac-Toe Game'
+          className="portfolio-image-popupbox"
+          src="images/tic-tac-toe.png"
+          alt="Tic-Tac-Toe Game"
         />
-        <h5 className='text-description'>
+        <h5 className="text-description">
           It is a simple Tic-Tac-Toe game where two players can play.
         </h5>
-        <b className='text-color'>Live Demo:</b>{' '}
+        <b className="text-color">Live Demo:</b>{" "}
         <a
-          className='hyper-link'
+          className="hyper-link"
           onClick={() =>
-            window.open('https://surendra1151.github.io/tic-tac-toe/')
+            window.open("https://surendra1151.github.io/tic-tac-toe/")
           }
         >
           https://surendra1151.github.io/tic-tac-toe/
         </a>
         <br />
-        <b className='text-color'>Github:</b>{' '}
+        <b className="text-color">Github:</b>{" "}
         <a
-          className='hyper-link'
+          className="hyper-link"
           onClick={() =>
-            window.open('https://github.com/surendra1151/tic-tac-toe')
+            window.open("https://github.com/surendra1151/tic-tac-toe")
           }
         >
           https://github.com/surendra1151/tic-tac-toe
@@ -155,47 +206,57 @@ const Portfolio = () => {
   const popupboxConfigTic = {
     titleBar: {
       enable: true,
-      text: 'Tic-Tac-Toe Game',
+      text: "Tic-Tac-Toe Game",
     },
     fadeIn: true,
     fadeInSpeed: 500,
   };
 
   return (
-    <div id='portfolio' className='portfolio-wrapper'>
-      <div className='container'>
-        <h1 className='text-uppercase text-center py-5'>Portfolio</h1>
-        <div className='image-box-wrapper row '>
-          <div className='portfolio-image-box' onClick={openPopupboxRestaurant}>
+    <div id="portfolio" className="portfolio-wrapper">
+      <div className="container">
+        <h1 className="text-uppercase text-center py-5">Portfolio</h1>
+        <div className="image-box-wrapper row ">
+          <div className="portfolio-image-box" onClick={openPopupboxNetflix}>
             <img
-              className='portfolio-image'
-              src='images/restaurant.png'
-              alt='Indian-Restaurant App'
+              className="portfolio-image"
+              src="images/netflix-image.png"
+              alt="Netflix Clone App"
             />
-            <div className='overflow'></div>
-            <FontAwesomeIcon className='portfolio-icon ' icon={faSearchPlus} />
+            <div className="overflow"></div>
+            <FontAwesomeIcon className="portfolio-icon " icon={faSearchPlus} />
+            <h4>Netflix Clone</h4>
+          </div>
+          <div className="portfolio-image-box" onClick={openPopupboxRestaurant}>
+            <img
+              className="portfolio-image"
+              src="images/restaurant.png"
+              alt="Indian-Restaurant App"
+            />
+            <div className="overflow"></div>
+            <FontAwesomeIcon className="portfolio-icon " icon={faSearchPlus} />
             <h4>Indian Restaurant</h4>
           </div>
 
-          <div className='portfolio-image-box' onClick={openPopupboxNotes}>
+          <div className="portfolio-image-box" onClick={openPopupboxNotes}>
             <img
-              className='portfolio-image note'
-              src='images/Note-taker.png'
-              alt='Note-Taker App'
+              className="portfolio-image note"
+              src="images/Note-taker.png"
+              alt="Note-Taker App"
             />
-            <div className='overflow-notes'></div>
-            <FontAwesomeIcon className='portfolio-icon ' icon={faSearchPlus} />
+            <div className="overflow-notes"></div>
+            <FontAwesomeIcon className="portfolio-icon " icon={faSearchPlus} />
             <h4>Note-Taker</h4>
           </div>
 
-          <div className='portfolio-image-box' onClick={openPopupboxTic}>
+          <div className="portfolio-image-box" onClick={openPopupboxTic}>
             <img
-              className='portfolio-image'
-              src='images/tic-tac-toe.png'
-              alt='Tic-Tac-Toe App'
+              className="portfolio-image"
+              src="images/tic-tac-toe.png"
+              alt="Tic-Tac-Toe App"
             />
-            <div className='overflow'></div>
-            <FontAwesomeIcon className='portfolio-icon ' icon={faSearchPlus} />
+            <div className="overflow"></div>
+            <FontAwesomeIcon className="portfolio-icon " icon={faSearchPlus} />
             <h4>Tic-Tac-Toe</h4>
           </div>
         </div>
